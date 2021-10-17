4db97aa38e92bc024e32b0deac0449ace3be6219
Bladtman <sigurt.bladt@gmail.com>
2021-10-17 10:39:40 +0200

    remove debug printing

--- dropout-position.js
+++ dropout-position.js
@@ -1,4 +1,3 @@
-const extname = "dropout-position";
 const url = window.location.pathname;
 
 const syncEveryMs = 1 * 60 * 1000; // 1 minute
@@ -8,10 +7,8 @@ let lastSave = Date.now();
 const save = (k, v) => new Promise(resolve => {
   const entry = {};
   entry[k] = v;
-  console.log(extname + ' set', v);
   chrome.storage.local.set(entry, resolve);
   if (lastSave < Date.now() - syncEveryMs) {
-    console.log(extname + ' sync', v);
     chrome.storage.sync.set(entry, resolve);
   }
 });
@@ -30,12 +27,9 @@ const  f = async () => {
   const player = VHX.Player('watch-embed');
   const savedTime = await get(url);
   if(savedTime) {
-    console.log(extname + ' seeking to', savedTime);
     player.currentTime(savedTime);
   }
   player.on('timeupdate', (e,time) => save(url, time));
 };
-
-console.log(extname, 'running');
 
 window.addEventListener('load', () => setTimeout(f, 400));
